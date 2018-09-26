2278419fa2341d8fc45e346539a3242eea3662af
aidan <aidandbush@gmail.com>
2018-09-25 18:38:57 -0600

    Add shell.c

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
+SHELL=/bin/bash
 CC=gcc
 CFLAGS=-Wall -std=c99 -pedantic -D_POSIX_C_SOURCE=200809L
 
 .PHONY: all clean
 
-all: q1 q2 q3 q7
+all: q1 q2 q3 q7 shell
 
 %: %.c
 
 clean:
-	$(RM) q{1,2,3,7} del
+	$(RM) q{1,2,3,7} shell del
---
+++ shell.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include <sys/wait.h>
+
+#define MAX_CMD_LEN 1024
+
+int main(void)
+{
+	bool done = false;
+	bool back;
+	int i, pid;
+	char *cmdline;
+	char *token = NULL;
+	char *args[10];
+
+	cmdline = calloc(1, MAX_CMD_LEN);
+
+	while (!done) {
+		i = 0;
+		back = false;
+
+		printf("prompt> ");
+		fgets(cmdline, MAX_CMD_LEN, stdin);
+
+		token = strtok(cmdline, "\n ");
+		while (token != NULL) {
+			args[i++] = strdup(token);
+			token = strtok(NULL, "\n ");
+		}
+
+		args[i] = NULL;
+
+		// if no command entered reprompt
+		if (i == 0)
+			continue;
+
+		// if put in background set back to true, and remove & from args
+		if (strnlen(args[i-1], MAX_CMD_LEN) == 1
+			&& args[i-1][0] == '&') {
+			back = true;
+			args[i-1] = NULL;
+		}
+
+		//fork
+		pid = fork();
+
+		if (pid < 0) {
+			// error
+			fprintf(stderr, "Cannot create process\n");
+			return 1;
+		} else if (pid == 0) {
+			// child
+			execvp(args[0], args);
+			return 1;
+		}
+		// parent
+
+		// check if need to wait for it to complete
+		if (!back)
+			wait(NULL);
+	}
+	free(cmdline);
+}
